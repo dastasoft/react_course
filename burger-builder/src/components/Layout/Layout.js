862afb3a98ceda631eae94dd22891510e5282aeb
KoshiroKun <kuroudo@protonmail.com>
2019-11-07 01:04:19 +0100

    adding a logo

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = props => (
-  <>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>{props.children}</main>
-  </>
+    <>
+        <Toolbar />
+        <main className={classes.Content}>{props.children}</main>
+    </>
 );
 
 export default layout;
