b20fa6638de0cf9368c6b801d1aa855f950cb381
KoshiroKun <kuroudo@protonmail.com>
2020-02-12 00:08:25 +0100

    show error messages on form validation

--- 03-burger-builder/src/components/UI/Input/Input.js
+++ 03-burger-builder/src/components/UI/Input/Input.js
@@ -4,29 +4,40 @@ import classes from './Input.module.css';
 
 const input = props => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
 
     switch (props.elementType) {
         case 'input':
             inputElement = (
                 <input
-                    className={classes.InputElement}
+                    className={inputClasses.join(' ')}
                     {...props.elementConfig}
                     value={props.value}
+                    onChange={props.changed}
                 />
             );
             break;
         case 'textarea':
             inputElement = (
                 <textarea
-                    className={classes.InputElement}
+                    className={inputClasses.join(' ')}
                     {...props.elementConfig}
                     value={props.value}
+                    onChange={props.changed}
                 />
             );
             break;
         case 'select':
             inputElement = (
-                <select className={classes.InputElement} value={props.value}>
+                <select
+                    className={inputClasses.join(' ')}
+                    value={props.value}
+                    onChange={props.changed}
+                >
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>
                             {option.displayValue}
@@ -38,18 +49,29 @@ const input = props => {
         default:
             inputElement = (
                 <input
-                    className={classes.InputElement}
+                    className={inputClasses.join(' ')}
                     {...props.elementConfig}
                     value={props.value}
+                    onChange={props.changed}
                 />
             );
             break;
+    }
+
+    let validationError = null;
+    if (props.invalid && props.touched) {
+        validationError = (
+            <p className={classes.ValidationError}>
+                Please enter a valid value.
+            </p>
+        );
     }
 
     return (
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     );
 };
--- 03-burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ 03-burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,12 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Your Name'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             street: {
                 elementType: 'input',
@@ -23,7 +28,12 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Street'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             zipCode: {
                 elementType: 'input',
@@ -31,7 +41,14 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'ZIP Code'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 5
+                },
+                valid: false,
+                touched: false
             },
             country: {
                 elementType: 'input',
@@ -39,7 +56,12 @@ class ContactData extends Component {
                     type: 'text',
                     placeholder: 'Country'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             email: {
                 elementType: 'input',
@@ -47,7 +69,12 @@ class ContactData extends Component {
                     type: 'email',
                     placeholder: 'Your E-Mail'
                 },
-                value: ''
+                value: '',
+                validation: {
+                    required: true
+                },
+                valid: false,
+                touched: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -66,9 +93,18 @@ class ContactData extends Component {
     orderHandler = event => {
         event.preventDefault();
         this.setState({ loading: true });
+        const formData = {};
+
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[
+                formElementIdentifier
+            ].value;
+        }
+
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price // calculate this on the server
+            price: this.props.price, // calculate this on the server
+            orderData: formData
         };
 
         axios
@@ -82,6 +118,44 @@ class ContactData extends Component {
             });
     };
 
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if (rules.required && isValid) {
+            isValid = value.trim() !== '';
+        }
+
+        if (rules.minLength && isValid) {
+            isValid = value.length >= rules.minLength;
+        }
+
+        if (rules.maxLength && isValid) {
+            isValid = value.length <= rules.maxLength;
+        }
+
+        return isValid;
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(
+            updatedFormElement.value,
+            updatedFormElement.validation
+        );
+        updatedFormElement.touched = true;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+        this.setState({ orderForm: updatedOrderForm });
+    };
+
     render() {
         const formElementsArray = Object.keys(this.state.orderForm).map(
             order => {
@@ -93,18 +167,22 @@ class ContactData extends Component {
         );
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {formElementsArray.map(formElement => (
                     <Input
                         key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
                         value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        touched={formElement.config.touched}
+                        changed={event =>
+                            this.inputChangedHandler(event, formElement.id)
+                        }
                     />
                 ))}
-                <Button btnType="Success" clicked={this.orderHandler}>
-                    ORDER
-                </Button>
+                <Button btnType="Success">ORDER</Button>
             </form>
         );
 
